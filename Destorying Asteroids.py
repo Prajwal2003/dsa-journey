e3ffcc4ca83492883da7e7700a100142dc1fd950
Prajwal Govindappa Hoolageri <prajwalgh13@gmail.com>
2026-05-31 15:31:28 +0530

    Sat 2

---
+++ Destorying Asteroids.py
@@ -0,0 +1,18 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        can_destory = mass
+        asteroids.sort()
+        for i in asteroids:
+            if can_destory >= i:
+                can_destory += i
+            else:
+                return False
+        return True
+    
+def main():
+    mass = 10
+    asteroids = [3,9,19,5,21]
+    print(Solution().asteroidsDestroyed(mass, asteroids))
+
+if __name__ == "__main__":
+    main()
