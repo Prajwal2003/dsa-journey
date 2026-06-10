e50770987dc992cb92cf209fc698bfd6601ea4c9
Prajwal Govindappa Hoolageri <prajwalgh13@gmail.com>
2026-06-10 20:50:31 +0530

    Just 1

---
+++ Reverse Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= (2**31 - 1) or x <= (-2**31):
+            return 0
+
+        rev = 0
+        for i in reversed(str(x)):
+            if i == "-":
+                rev = 0 - rev
+            else:
+                rev = rev * 10 + int(i)
+
+        if rev >= (2**31 - 1) or rev <= (-2**31):
+            return 0
+        return rev
+
+def main():
+    sol = Solution()
+    print(sol.reverse(123))
+
